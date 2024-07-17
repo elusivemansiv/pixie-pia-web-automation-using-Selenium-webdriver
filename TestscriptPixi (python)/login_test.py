38f2bbe824776d16b83f694c7b50bac1ed3fd917
Khan,Mansib  Ahsan <97165527+elusivemansiv@users.noreply.github.com>
2024-07-17 12:02:21 +0600

    Update login_test.py

--- TestscriptPixi (python)/login_test.py
+++ TestscriptPixi (python)/login_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.service import Service
 
+excute_path = ("C:\\Driver\\chromedriver-win64\\chromedriver.exe")
+service = Service(excute_path)
+driver = webdriver.Chrome(service=service)
 
-driver = webdriver.Chrome()
 driver.get("https://pixie-pia.web.app/login")
 
 time.sleep(5)
@@ -19,3 +22,9 @@ login_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div/div/f
 login_button.click()
 
 time.sleep(5)
+
+logout_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[3]/a/button')
+logout_button.click()
+time.sleep(5)
+
+driver.quit()
